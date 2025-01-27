4fe86a8832b8662258187e178db3b449b66fadfb
Asu <51758@yupparaj.ac.th>
2025-01-27 17:42:13 +0700

    Update Line_Tracking.ino

--- Line_Tracking/Line_Tracking.ino
+++ Line_Tracking/Line_Tracking.ino
@@ -1,5 +1,8 @@
 #include <POP32.h>
+#include <U8g2lib.h>  // ใช้ U8g2
+#include <Wire.h>     // จำเป็นสำหรับการสื่อสาร I2C
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 int FL, C, FR, BL, BR;
 int RefFL = 1900,
     RefC  = 2435,
@@ -14,16 +17,14 @@ const int ZX_SWITCH_PIN = 22;  // ต่อสวิตช์กับ D22 (TX-2
 
 void setup() {
   pinMode(ZX_SWITCH_PIN, INPUT_PULLUP);  // เปิดโหมด Pull-up สำหรับสวิตช์
+  u8g2.begin();  // เริ่มต้นจอ OLED
 }
 
 void loop() {
-  // ตรวจสอบการกด ZX_SWITCH01
-  if (digitalRead(ZX_SWITCH_PIN) == LOW) {  // ถ้ากดสวิตช์ (สัญญาณเป็น LOW)
-    while (digitalRead(ZX_SWITCH_PIN) == LOW);  // รอจนกว่าปุ่มจะถูกปล่อย
-    // Forward(60,3000);
-    // TrackJC(60, 1000);  // เริ่มทำงานติดเส้น
-    TrackJC(45,3000);
-    MotorStop();        // หยุดหลังทำงานเสร็จ
+  if (digitalRead(ZX_SWITCH_PIN) == LOW) {
+    while (digitalRead(ZX_SWITCH_PIN) == LOW);
+    TrackJC(60, 1000);
+    MotorStop();
   }
-
+  ShowData();
 }
