3d3c773afe0f1cf5a0f0dce8d752a043085ced36
Asu <51758@yupparaj.ac.th>
2025-01-30 11:05:50 +0700

    Update Analog.ino

--- Line_Tracking/Analog.ino
+++ Line_Tracking/Analog.ino
@@ -1,17 +1,30 @@
 void ReadAnalog(){ //อ่านค่าเซนเซอร์ข้างหน้า
-  FL = analog(0);     // FrontLeft
-  C = analog(1);      // Middle
-  FR = analog(2);     // FrontRight
-  BL = analog(3);     // BackLeft
-  BR = analog(4);     // BackRight
+   L3 = analog(0);  
+   L2 = analog(1);
+   L1 = analog(2);
+   C  = analog(3);
+   R1 = analog(4);
+   R2 = analog(5);
+   R3 = analog(6);
 }
 
-void ConvertAnalog(){     // Black = 0, White = 1
+void ConvertAnalog() 
+{     
+  // Black = 0, White = 1
   ReadAnalog();
   // ถ้า analog(int) > ค่าเฉลี่ยของ analog(int) --> ananlog(int) = 0 (สีดำ) | ไม่ใช่ --> analog(int) = 1 (สีขาว1)
-  if(FL < RefFL) FL=0; else FL = 1;
-  if(C  < RefC)  C=0;  else C  = 1;
-  if(FR < RefFR) FR=0; else FR = 1;
-  if(BL < RefBL) BL=0; else BL = 1;
-  if(BR < RefBR) BR=0; else BR = 1;
+
+  // Left 
+  if(L3 < RefL3) L3 = 0;  else L3 = 1;
+  if(L2 < RefL2) L2 = 0;  else L2 = 1;
+  if(L1 < RefL1) L1 = 0;  else L1 = 1;
+
+  // Middle 
+  if(C  < RefC ) C  = 0;  else C  = 1;
+
+  // Right
+  if(R1 < RefR1) R1 = 0;  else R1 = 1;
+  if(R2 < RefR2) R2 = 0;  else R2 = 1;
+  if(R3 < RefR3) R3 = 0;  else R3 = 1;
+  
 }
