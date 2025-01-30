8a54418496b1d7b6d9157b8d8eb9194acccfd3bb
Asu <51758@yupparaj.ac.th>
2025-01-30 11:08:58 +0700

    Update PID.ino

--- Line_Tracking/PID.ino
+++ Line_Tracking/PID.ino
@@ -1,43 +1,51 @@
 void CalError()
 {
   ConvertAnalog();
-  if      (FL == 1 && C == 1 && FR == 0) Error =  2;   // 1 1 0  
-  else if (FL == 1 && C == 0 && FR == 0) Error =  1;   // 1 0 0
-  else if (FL == 1 && C == 0 && FR == 1) Error =  0;   // 1 0 1
-  else if (FL == 0 && C == 0 && FR == 1) Error = -1;   // 0 0 1
-  else if (FL == 0 && C == 1 && FR == 1) Error = -2;   // 0 1 1
+  if      (L3 == 1 && L2 == 1 && L1 == 1 && C == 1 && R1 == 1 && R2 == 1 && R3 == 0) Error = 6;  //  1  1  1  1  1  1  0
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 1 && R1 == 1 && R2 == 0 && R3 == 0) Error = 5;  //  1  1  1  1  1  0  0 
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 1 && R1 == 1 && R2 == 0 && R3 == 1) Error = 4;  //  1  1  1  1  1  0  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 1 && R1 == 0 && R2 == 0 && R3 == 1) Error = 3;  //  1  1  1  1  0  0  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 1 && R1 == 0 && R2 == 1 && R3 == 1) Error = 2;  //  1  1  1  1  0  1  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 0 && R1 == 0 && R2 == 1 && R3 == 1) Error = 1;  //  1  1  1  0  0  1  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 1 && C == 0 && R1 == 1 && R2 == 1 && R3 == 1) Error = 0;  //  1  1  1  0  1  1  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 0 && C == 0 && R1 == 1 && R2 == 1 && R3 == 1) Error = -1; //  1  1  0  0  1  1  1 
+  else if (L3 == 1 && L2 == 1 && L1 == 0 && C == 1 && R1 == 1 && R2 == 1 && R3 == 1) Error = -2; //  1  1  0  1  1  1  1 
+  else if (L3 == 1 && L2 == 0 && L1 == 0 && C == 1 && R1 == 1 && R2 == 1 && R3 == 1) Error = -3; //  1  0  0  1  1  1  1 
+  else if (L3 == 1 && L2 == 0 && L1 == 1 && C == 1 && R1 == 1 && R2 == 1 && R3 == 1) Error = -4; //  1  0  1  1  1  1  1 
+  else if (L3 == 0 && L2 == 0 && L1 == 1 && C == 1 && R1 == 1 && R2 == 1 && R3 == 1) Error = -5; //  0  0  1  1  1  1  1 
+  else if (L3 == 0 && L2 == 1 && L1 == 1 && C == 1 && R1 == 1 && R2 == 1 && R3 == 1) Error = -6; //  0  1  1  1  1  1  1 
 
-  else if (FL == 0 &&  C == 0 && FR == 0) Error = 100; // 0 0 0 
-  else if (FL == 0 &&  C == 0) Error = 101;            // 0 0 ?
-  else if (C  == 0 && FR == 0) Error = 102;            // ? 0 0 
+  else if (L3 == 0 && L2 == 0 && L1 == 0 && C == 0 && R1 == 0 && R2 == 0 && R3 == 0) Error = 100; //  0  0  0  0  0  0  0 
+  else if (L3 == 0 && L2 == 0 && L1 == 0) Error = 101;     // 0 0 0 ? ? ? ?
+  else if (R1 == 0 && R2 == 0 && R3 == 0) Error = 102;     // ? ? ? ? 0 0 0
 }
 
 void TrackPID(int MotorSpeed)
 {
   int Output,LeftOutput, RightOutput, MaxSpeed = MotorSpeed;
-  CalError();
+
   TuneMotor(MotorSpeed); 
   Integral += Error;    // Integral = Integral + Error; 
   Derivative = Error - PreError;
   Output = (Kp * Error) + (Ki * Integral) + (Kd * Derivative);
-  
-  if (Error == -2) {
-    LeftOutput  = (LeftSpeed  + Output)+50;
-    RightOutput = (RightSpeed - Output)-20;
-  } 
-  else {
-    LeftOutput  = (LeftSpeed  + Output)+30;
-    RightOutput = (RightSpeed - Output)+10;
-  }
+  LeftOutput  = (LeftSpeed  + Output);
+  RightOutput = (RightSpeed - Output);
+  // if (Error == -2) {
+  //   LeftOutput  = (LeftSpeed  + Output)+50;
+  //   RightOutput = (RightSpeed - Output)-20;
+  // } 
+  // else {
+  //   LeftOutput  = (LeftSpeed  + Output)+30;
+  //   RightOutput = (RightSpeed - Output)+10;
+  // }
 
   constrain(LeftOutput,-MaxSpeed,MaxSpeed);
   constrain(RightOutput,-MaxSpeed,MaxSpeed);
       
-  motor(1,RightOutput);
-  motor(3,RightOutput);
-    
-  motor(2,LeftOutput);
-  motor(4,LeftOutput);
+  motor(,RightOutput);
+  motor(,RightOutput);
+  motor(,LeftOutput);
+  motor(,LeftOutput);
   // motor(2,-LeftOutput);
   // motor(4,-LeftOutput);
   
@@ -47,6 +55,7 @@ void TrackPID(int MotorSpeed)
 void TrackJC(int MotorSpeed, int Time) // เดินเช็คแยก
 {
   int Error = 0;
+  int PreError = 0; 
   while(Error < 100)
   {
     TrackPID(MotorSpeed);
